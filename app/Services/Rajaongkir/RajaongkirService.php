b700ee66f533b2d67e8c8ae61984cf3595766fbd
Fikri Suheri <61069138+fikrisuheri@users.noreply.github.com>
2022-07-28 10:31:52 +0700

    Update RajaongkirService.php

--- app/Services/Rajaongkir/RajaongkirService.php
+++ app/Services/Rajaongkir/RajaongkirService.php
@@ -26,27 +26,32 @@ class RajaongkirService
 
     protected function apiCall(string $urlPath, string $field = "", string $method = 'GET')
     {
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $this->url . $urlPath,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => $method,
-            CURLOPT_POSTFIELDS => $field,
-            CURLOPT_HTTPHEADER => array(
-                "key: " . $this->config['api_key']
-            ),
-        ));
+        try {
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $this->url . $urlPath,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => $method,
+                CURLOPT_POSTFIELDS => $field,
+                CURLOPT_HTTPHEADER => array(
+                    "key: " . $this->config['api_key']
+                ),
+            ));
 
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
 
-        curl_close($curl);
-        $data = json_decode($response,true);
-        return $data['rajaongkir']['results'];
+            curl_close($curl);
+            $data = json_decode($response,true);
+            return $data['rajaongkir']['results'];
+        } catch (\Throwable $th) {
+            dd($th->getMessage());
+        }
+        
     }
 
     public function getProvince()
